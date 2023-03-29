e27ca3004146cd1d1b3d60088e3098c33d8e0e6b
ayansheikh6600 <ayansheikh6600@email.com>
2023-03-29 23:35:02 +0500

    done

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 function setNumber(num) {
-    var input = document.getElementById("number")
+    var input = document.getElementById("input")
     input.value += num
 }
 function ans() {
-    var inputValue = document.getElementById("number")
+    var inputValue = document.getElementById("input")
     var output = eval(inputValue.value)
     inputValue.value = output
+}
+function allclear(){
+    var input = document.getElementById("input")
+    input.value = ""
+}
+function clear(){
+    
+console.log("input")
+    // input += input.slice(0,-1) 
+
 }
